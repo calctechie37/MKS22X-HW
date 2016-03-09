6041d9156eeb5d09e858447850407d02536c14c9
Patrick Chan <patrickchan1999@gmail.com>
2016-03-09 12:49:08 -0500

    Missed a line in partition

--- 07QuickSelect/QuickSelect.java
+++ 07QuickSelect/QuickSelect.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 public class QuickSelect{
     
-    public int QuickSelect(int[] data, int k){
+    public static int QuickSelect(int[] data, int k){
 	return QuickSelect(data, 0, data.length - 1, k - 1);
     }
 
-    private int QuickSelect(int[] data, int first, int last, int k) {
+    private static int QuickSelect(int[] data, int first, int last, int k) {
 	if (first <= last) {
+	    
 	    int pivot = partition(data, first, last);
-	    if (pivot == k) {
-		return data[k];
+
+	    if (k == pivot) {
+		return data[pivot];
 	    }
-	    if (pivot > k) {
+	    if (k < pivot) {
 		return QuickSelect(data, first, pivot - 1, k);
 	    }
 	    return QuickSelect(data, pivot + 1, last, k);
@@ -20,12 +22,13 @@ public class QuickSelect{
 	return Integer.MIN_VALUE;
     }
  
-    private int partition(int[] data, int first, int last) {
+    private static int partition(int[] data, int first, int last) {
 	int pivot = first + new Random().nextInt(last - first + 1);
-	swap(data, last, pivot);
+	int pivotValue = data[pivot];
+	swap(data, pivot, last);
 	for (int i = first; i < last; i++) {
-	    if (data[i] > data[last]) {
-		swap(data, i, first);
+	    if (data[i] <= pivotValue) {
+		swap(data, first, i);
 		first++;
 	    }
 	}
@@ -33,9 +36,30 @@ public class QuickSelect{
 	return first;
     }
  
-    private void swap(int[] data, int x, int y) {
+    private static void swap(int[] data, int x, int y) {
 	int tmp = data[x];
 	data[x] = data[y];
 	data[y] = tmp;
+    }
+    
+    public static void main(String[] args){
+	int n;
+	if (args.length > 0){
+	    n = Integer.parseInt(args[0]);
+	} else{
+	    n = 10;
+	}
+	int [] a = new int[n];
+	Random r = new Random();
+	for(int i = 0; i < n; i++){
+	    a[i] = r.nextInt(2000000) - 1;
+	}
+	
+	System.out.println(Arrays.toString(a));
+	System.out.println(QuickSelect(a, 3));
+	System.out.println(Arrays.toString(a));
+	Arrays.sort(a);
+	System.out.println(Arrays.toString(a));
+	System.out.println(a[2]);
     }
 }
